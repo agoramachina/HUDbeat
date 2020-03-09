009039fe6541d231bd619f85436f6f3ff936e436
agoramachina <agoramachina@gmail.com>
2020-03-09 00:29:19 -0400

    rerange midi

--- mindwave/midimind/midimindpowers.py
+++ mindwave/midimind/midimindpowers.py
@@ -16,6 +16,44 @@ def get_samples(samples=30):
         dfq = pd.read_csv(io.StringIO('\n'.join(q)))
         return dfq
 
+class DataPoints():
+    def __init__ (self, data):
+      self.times = data.iloc[:,0]
+      self.signals = data.iloc[:,1]
+
+      self.attns = data.iloc[:,2]
+      self.meds = data.iloc[:,3]
+
+      self.powers = self.Powers(data.iloc[:,4:12])
+
+class Powers():
+    def __init__ (self,powers):
+      self.deltas = self.powers.values[:,0]
+      self.thetas = self.powers.values[:,1]
+      self.l_alphas = self.powers.values[:,2]
+      self.h_alphas = self.powers.values[:,3]
+      self.l_betas = self.powers.values[:,4]
+      self.h_betas = self.powers.values[:,5]
+      self.l_gammas = self.powers.values[:,6]
+      self.m_gammas = self.powers.values[:,7]
+
+# power stats
+class PowerStats():
+    def __init__ (self, powers):
+        self.mins = np.log(powers.min().values)
+        self.maxs = np.log(powers.max().values)
+        self.means = np.log(powers.mean().values)
+        self.ranges = maxs - mins
+        self.difs = np.log(powers.iloc[[0,-1]].values) - np.log(powers.iloc[[0,-2]].values)
+
+# scale power ranges
+def rerange(powers):
+    mins = np.log(powers.min().values)
+    maxs = np.log(powers.max().values)
+    ranges = maxs - mins
+
+    #reranges =
+
 # play notebuffer
 def play_arp(midiout,notes):
 
@@ -24,6 +62,10 @@ def play_arp(midiout,notes):
       midiout.send_message([0x90, note, velocity])
       time.sleep(.5/len(notes))
       midiout.send_message([0x80, note, 0])
+
+def play_solo(midiout,pow):
+    midiout.send_message([0x90, note, velocity])
+
 
 def play(midiout,pow):
     if (pow < 9):
@@ -42,7 +84,7 @@ def play(midiout,pow):
 def main():
 
     midiout = []
-    
+
     for i in range(0,8):
         midiout.append(rtmidi.MidiOut(b'rtmidi pow'))
         midiout[i].open_port(i+1)
@@ -51,28 +93,28 @@ def main():
       try:
           samples = 30
           data = get_samples(samples)
-          rows, cols = data.shape
-          
-                   
+          #rows, cols = data.shape
+
+
           signal = data.iloc[:,1]
           powers = data.iloc[:,4:12]
 
-          mins = np.log(powers.min().values)
-          maxs = np.log(powers.max().values)
-          means = np.log(powers.mean().values)
-          ranges = maxs - mins
-          difs = np.log(powers.iloc[[0,-1]].values) - np.log(powers.iloc[[0,-2]].values)
-                    
+          #mins = np.log(powers.min().values)
+          #maxs = np.log(powers.max().values)
+          #means = np.log(powers.mean().values)
+          #ranges = maxs - mins
+          #difs = np.log(powers.iloc[[0,-1]].values) - np.log(powers.iloc[[0,-2]].values)
+
           #print(np.log(powers.values[0]).round(decimals=3))
           #print("%2.3f" %np.log(powers.values[0,0]))
           for pow in powers.values[0]:
               print("%2.3f" %np.log(pow), end='\t')
           print()
-          
+
           #midiout[0].send_message([0x90,60,127])
 
           play(midiout[0],np.log(powers.values[0,0]))
-                    
+
       except (KeyboardInterrupt):
           for midi in midiout:
               midi.send_message([120]) # all sound off
@@ -92,5 +134,3 @@ if __name__ == '__main__':
     #header = list(df)
 
     main()
-
-
