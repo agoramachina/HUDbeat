5d1b3c0bf4d70a51a4bbe78a704cce474c4d4d7e
agoramachina <agoramachina@gmail.com>
2021-04-13 14:22:42 -0400

    rtmidi

--- mindwave/midimind/midimind.py
+++ mindwave/midimind/midimind.py
@@ -17,6 +17,30 @@ def get_samples(samples=1):
         dfv = dfq.values
         return dfq
 
+class MidiOutWrapper:
+
+  def __init__(self, midi, ch=1):
+    self.channel = ch
+    self._midi = midi
+    
+  def channel_message(self, command, *data, ch=None):
+      """Send a MIDI channel mode message."""
+      command = (command & 0xf0) | ((ch if ch else self.channel) - 1 & 0xf)
+      msg = [command] + [value & 0x7f for value in data]
+      self._midi.send_message(msg)
+      
+  def note_off(self, note, velocity=0, ch=None):
+    """Send a 'Note Off' message."""
+    self.channel_message(NOTE_OFF, note, velocity, ch=ch)
+    
+  def note_on(self, note, velocity=127, ch=None):
+    """Send a 'Note On' message."""
+    self.channel_message(NOTE_ON, note, velocity, ch=ch)
+    
+  def program_change(self, program, ch=None):
+   """Send a 'Program Change' message."""
+   self.channel_message(PROGRAM_CHANGE, program, ch=ch)
+
 # play notebuffer
 def play_arp(midiout,notes):
 
@@ -72,8 +96,9 @@ def main():
     if available_ports:
         midiout.open_port(0)
     else:
-        midiout.open_virtual_port(b'rtmidi viritual midi')
+        midiout.open_virtual_port('rtmidi viritual midi')
 
+    #midiout = MidiOutWrapper(midiout)
 
     while(True):
       try:
