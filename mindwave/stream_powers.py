924e3c1181048412bd990dfe29260f83fbf931f0
agoramachina <agoramachina@gmail.com>
2021-08-14 19:05:46 -0400

    tui stream

--- mindwave/stream_powers.py
+++ mindwave/stream_powers.py
@@ -3,12 +3,11 @@ import numpy as np
 import plotext as plx
 import recordEEG as eeg
 
-samples = 30
 colors = ['red', 'gold', 'green', 'basil', 'indigo', 'blue', 'lilac', 'violet']
 
 while True:
 
-  data = eeg.Datapoints(eeg.get_samples(samples))
+  data = eeg.Datapoints(eeg.get_samples(samples = 30))
   plx.clear_plot()
   plx.clear_terminal()
 
@@ -18,16 +17,11 @@ while True:
 
   plx.ticks(0,0)
   plx.xaxes(False)
-  #plx.yaxes(False)
+  plx.yaxes(False)
 
 
-  yt = []
   for i in range(8):
       plx.plot(data.times, data.powers[i], color=colors[i], marker = "•")
-      yt.append(data.powers[i][0])
-  yt.reverse()
-  plx.yticks(yt)
-
-      
+            
   plx.show()
   time.sleep(1)
