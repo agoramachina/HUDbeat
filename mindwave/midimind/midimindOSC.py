d0276ffd478ea8348cad26ad69de5d632b240a61
agoramachina <agoramachina@gmail.com>
2021-08-21 20:56:35 -0400

    change tail

--- mindwave/midimind/midimindOSC.py
+++ mindwave/midimind/midimindOSC.py
@@ -1,15 +1,56 @@
+import time, datetime, glob, os, sys, io
+from optparse import OptionParser
+from collections import deque
+import numpy as np
+
 from pythonosc import udp_client
 from pythonosc import osc_message_builder
-from pythonosc.udp_client import SimpleUDPClient
 
+# OS friendly formatting
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# fond most recent folder and file
+dir = max([f.path for f in os.scandir('../EEG_data/') if f.is_dir()])
+file = max(glob.glob(os.path.join(dir, 'EEGlog_*.csv')),key=os.path.getctime)
+
+SLEEP_INTERVAL = 1.0
 
 ip = '127.0.0.1'
 port = 57121
 
 
 sender = udp_client.SimpleUDPClient(ip, port)
-sender.send_message('/trigger/prophet', [70, 100, 8])
+#while True:
+#    sender.send_message('/pow', [70, 100, 8, 100])
 
-#client = SimpleUDPClient(ip, port) # Create client
-#client.send_message(ip, 123) # Send float message
-#client.send_message(1p, [1, 2., "hello"]) # Send message with int, float and string
+
+data = np.genfromtxt(file, dtype=int, delimiter=',', names=True)
+print(data)
+ 
+def tail(f):
+    while True:
+      where = f.tell()
+      line = f.readline()
+      if not line:
+          time.sleep(SLEEP_INTERVAL)
+          f.seek(where)
+      else:
+          yield (line)
+
+def read_tail():
+    with open (file, 'r') as f:
+      for line in tail(f):
+          line = line.strip()
+          values = [i for i in line.split(',')]
+          array = np.array(values)
+
+          powers = array[4:len(array)]
+          for pow in powers:
+            pow = float(pow)
+          print (powers)
+
+          #for i in range (4, len(array)):
+          #    array[i] = np.log(i)
+          #    powers = array[i]
+        
+          #print(array)
