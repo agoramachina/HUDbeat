3fe1b019bfd17d285de5da6bd988726a4bbacbc4
agoramachina <agoramachina@gmail.com>
2022-07-13 21:51:06 -0400

    fixed merge

---
+++ mindwave/stream_pow.py
@@ -0,0 +1,41 @@
+# although EEG band power units are typically represented by V²/Hz,
+# the values reported by the mindwave have undergone transformations
+# such that there is no longer a simple voltage correlation.
+# reported values are meaningful relative to each other over time,
+# but should not be considered to have formal units.
+# 
+# for more details, see:
+# http://support.neurosky.com/kb/development-2/eeg-band-power-values-units-amplitudes-and-meaning
+ 
+import time
+import numpy as np
+import plotext as plx
+import recordEEG as eeg
+
+#colors = ['red', 'orange', 'green', 'basil', 'indigo', 'blue', 'lilac', 'violet']
+
+colors = [197, 214, 154, 28, 45, 27, 165, 90]
+
+
+while True:
+
+  try:
+    data = eeg.Datapoints(eeg.get_samples(samples = 30))
+  except(ValueError, IndexError):
+    data = eeg.Datapoints(eeg.get_samples())
+    
+  plx.clear_figure()
+  plx.clear_terminal()
+  plx.canvas_color('black')  
+
+  plx.yscale('log')
+  plx.xticks([])
+  plx.yticks([])
+  plx.xaxes(False, False)
+  plx.yaxes(False, False)
+
+  for i in range(8):
+      plx.plot(data.times, data.powers[i], color=colors[i], marker = "•")
+            
+  plx.show()
+  time.sleep(1)
---
+++ mindwave/stream_raw.py
@@ -0,0 +1,26 @@
+import time
+import numpy as np
+import plotext as plx
+import recordEEG as eeg
+
+while True:
+
+  data = eeg.get_raw(samples = 240)
+  
+  plx.clear_figure()
+  plx.clear_terminal()
+  plx.clear_color()
+
+  y=(data)
+  ymax = 1800
+  
+  plx.xaxes(False, False)
+  plx.yaxes(True, False)
+  yticks = [-ymax, -ymax/2, 0, ymax/2, ymax]
+  ylabels = [-ymax, int(-ymax/2), "0 µV ", int(ymax/2), ymax]
+ 
+  plx.xticks([])
+  plx.yticks(yticks, ylabels)
+  plx.ylim(-ymax,ymax)
+  plx.plot(y)
+  plx.show()
