6bf8a7cc68fe8a8a27f25fb3800cc9dc302c1443
agoramachina <agoramachina@gmail.com>
2022-08-12 05:47:20 -0400

    supercollider midi stukff

--- mindwave/midimind/eeg2osc.py
+++ mindwave/midimind/eeg2osc.py
@@ -14,7 +14,7 @@ file = max(glob.glob(os.path.join(dir, 'EEGlog_*.csv')),key=os.path.getctime)
 
 # define ip and port
 ip = '192.168.1.88'
-port = 57120	#sc
+port = 57127	#sc
 #port = 4560	#sp
 
 # setup OSC server
@@ -28,13 +28,13 @@ while True:
 
   line = data[-1]
   for i in range(4, 12):
-    line[i] = np.log(line[i]) #np.round(np.log(line[i]))
+    line[i] = np.round(np.log(line[i]), 3) #np.log(line[i]) #np.round(np.log(line[i]))
   print (line)
 
   
     
   # Build and send OSC message
-  msg = osc_message_builder.OscMessageBuilder(address = 'eeg')
+  msg = osc_message_builder.OscMessageBuilder(address = '/eeg')
   for value in line:   
       msg.add_arg(value, arg_type='f')
   msg = msg.build()
